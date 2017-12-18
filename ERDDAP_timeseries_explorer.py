288153b8fbecb3e1bc4e5c1ced3a32f48fd7b6d1
rsignell-usgs <rsignell@usgs.gov>
2017-12-18 10:48:56 -0500

    new ts

--- ERDDAP_timeseries_explorer.py
+++ ERDDAP_timeseries_explorer.py
@@ -5,24 +5,32 @@
 # Inspired by [Jason Grout's excellent ESIP Tech Dive talk on "Jupyter Widgets"](https://youtu.be/CVcrTRQkTxo?t=2596), this notebook uses the `ipyleaflet` and `bqplot` widgets
 # to interactively explore the last two weeks of time series data from an ERDDAP Server. Select a `standard_name` from the list, then click a station to see the time series.  
 
-# In[ ]:
+# NOTE: To access a protected ERDDAP endpoint is protected, you can add a `~/.netrc` file like this:
+# ```
+# machine cgoms.coas.oregonstate.edu
+# login <username>
+# password <password>
+# ```
+
+# In[1]:
 
 import numpy as np
 import pandas as pd
+import requests
+from io import StringIO
 
 
 # `pendulum` is a drop-in replacement for `datetime`, with more useful functions
 
-# In[ ]:
+# In[2]:
 
 import pendulum
 import datetime
-global min_time, max_time
 
 
 # `ipyleaflet` and `bqplot` are both Jupyter widgets, so can interact with Python like any other widget.  Since we want to click on a map in a notebook and get an interactive time series plot, they are perfect tools to use here. 
 
-# In[ ]:
+# In[3]:
 
 import ipyleaflet as ipyl
 import bqplot as bq
@@ -31,29 +39,14 @@ import ipywidgets as ipyw
 
 # To make working with ERDDAP simpler, we use `erddapy`, a high-level python interface to ERDDAP's RESTful API
 
-# In[ ]:
+# In[4]:
 
 from erddapy import ERDDAP
 
 
 # This code should work with minor modifications on any ERDDAP (v1.64+) endpoint that has `cdm_data_type=timeseries` or `cdm_data_type=point` datasets.  Change the values for other ERDDAP endpoints or regions of interest
 
-# In[ ]:
-
-endpoint = 'https://erddap-uncabled.oceanobservatories.org/uncabled/erddap'
-
-initial_standard_name = 'sea_water_temperature'
-
-nchar = 8 # number of characters for short dataset name
-cdm_data_type = 'Point'
-center = [35, -100]
-zoom = 1
-
-min_time = '2017-08-01T00:00:00Z'
-max_time = '2017-08-03T00:00:00Z'
-
-
-# In[ ]:
+# In[5]:
 
 endpoint = 'http://erddap.sensors.ioos.us/erddap'
 
@@ -68,23 +61,7 @@ min_time = pendulum.parse('2017-11-01T00:00:00Z')
 max_time = pendulum.parse('2017-11-11T00:00:00Z')
 
 
-# In[ ]:
-
-endpoint = 'http://ooivm1.whoi.net/erddap'
-
-initial_standard_name = 'wind_speed'
-
-nchar = 3 # number of characters for short dataset name
-cdm_data_type = 'TimeSeries'
-center = [42.5, -68]
-zoom = 6
-
-now = pendulum.utcnow()
-max_time = now
-min_time = now.subtract(weeks=2) 
-
-
-# In[ ]:
+# In[6]:
 
 endpoint = 'http://www.neracoos.org/erddap'
 
@@ -100,7 +77,7 @@ max_time = now
 min_time = now.subtract(weeks=2) 
 
 
-# In[ ]:
+# In[7]:
 
 endpoint = 'https://gamone.whoi.edu/erddap'
 
@@ -115,28 +92,76 @@ min_time = pendulum.parse('2011-05-05T00:00:00Z')
 max_time = pendulum.parse('2011-05-15T00:00:00Z')
 
 
-# In[ ]:
+# In[8]:
+
+endpoint = 'https://erddap-uncabled.oceanobservatories.org/uncabled/erddap'
+
+initial_standard_name = 'sea_water_temperature'
+
+nchar = 8 # number of characters for short dataset name
+cdm_data_type = 'Point'
+center = [35, -100]
+zoom = 1
+
+min_time = pendulum.parse('2017-08-01T00:00:00Z')
+max_time = pendulum.parse('2017-08-03T00:00:00Z')
+
+
+# In[9]:
+
+endpoint = 'https://cgoms.coas.oregonstate.edu/erddap'
+
+initial_standard_name = 'air_temperature'
+
+nchar = 8 # number of characters for short dataset name
+cdm_data_type = 'TimeSeries'
+center = [44, -124]
+zoom = 6
+
+now = pendulum.utcnow()
+max_time = now
+min_time = now.subtract(weeks=1) 
+
+
+# In[10]:
+
+endpoint = 'http://ooivm1.whoi.net/erddap'
+
+initial_standard_name = 'solar_panel_1_voltage'
+
+nchar = 8 # number of characters for short dataset name
+cdm_data_type = 'TimeSeries'
+center = [41.0, -70.]
+zoom = 7
+
+now = pendulum.utcnow()
+max_time = now
+min_time = now.subtract(days=3) 
+
+
+# In[11]:
 
 e = ERDDAP(server_url=endpoint)
 
 
 # Find all the `standard_name` attributes that exist on this ERDDAP endpoint, using [ERDDAP's "categorize" service](http://www.neracoos.org/erddap/categorize/index.html)
 
-# In[ ]:
+# In[12]:
 
 url='{}/categorize/standard_name/index.csv'.format(endpoint)
-df = pd.read_csv(url, skiprows=[1, 2])
+r = requests.get(url, verify=True)
+df = pd.read_csv(StringIO(r.text), skiprows=[1, 2])
 vars = df['Category'].values
 
 
 # Create a dropdown menu widget with all the `standard_name` values found
 
-# In[ ]:
+# In[13]:
 
 dpdown = ipyw.Dropdown(options=vars, value=initial_standard_name)
 
 
-# In[ ]:
+# In[14]:
 
 start_time = ipyw.DatePicker(
     description='Start Date',
@@ -145,7 +170,7 @@ start_time = ipyw.DatePicker(
 )
 
 
-# In[ ]:
+# In[15]:
 
 stop_time = ipyw.DatePicker(
     description='Stop Date',
@@ -156,15 +181,16 @@ stop_time = ipyw.DatePicker(
 
 # This function convert an ERDDAP timeseries CSV response to a Pandas dataframe
 
-# In[ ]:
+# In[16]:
 
 def download_csv(url):
-    return pd.read_csv(url, index_col='time', parse_dates=True, skiprows=[1])
+    r = requests.get(url, verify=True)
+    return pd.read_csv(StringIO(r.text), index_col='time', parse_dates=True, skiprows=[1])
 
 
 # This function puts lon,lat and datasetID into a GeoJSON feature
 
-# In[ ]:
+# In[17]:
 
 def point(dataset, lon, lat, nchar):
     geojsonFeature = {
@@ -184,14 +210,14 @@ def point(dataset, lon, lat, nchar):
 
 # This function finds all the datasets with a given standard_name in the specified time period, and return GeoJSON
 
-# In[ ]:
+# In[18]:
 
 def adv_search(e, standard_name, cdm_data_type, min_time, max_time):
     try:
         search_url = e.get_search_url(response='csv', cdm_data_type=cdm_data_type.lower(), items_per_page=100000,
                                   standard_name=standard_name, min_time=min_time, max_time=max_time)
-        print(search_url)
-        df = pd.read_csv(search_url)
+        r = requests.get(search_url, verify=True)
+        df = pd.read_csv(StringIO(r.text))
     except:
         df = []
         if len(var)>14:
@@ -205,15 +231,16 @@ def adv_search(e, standard_name, cdm_data_type, min_time, max_time):
 
 # This function returns the lon,lat vlaues from allDatasets
 
-# In[ ]:
+# In[19]:
 
 def alllonlat(e, cdm_data_type, min_time, max_time):
     url='{}/tabledap/allDatasets.csv?datasetID%2CminLongitude%2CminLatitude&cdm_data_type=%22{}%22&minTime%3C={}&maxTime%3E={}'.format(e.server_url,cdm_data_type,max_time,min_time)
-    df = pd.read_csv(url, skiprows=[1])
+    r = requests.get(url, verify=True)
+    df = pd.read_csv(StringIO(r.text), skiprows=[1])
     return df
 
 
-# In[ ]:
+# In[20]:
 
 def stdname2geojson(e, standard_name, min_time, max_time, nchar):
     '''return geojson containing lon, lat and datasetID for all matching stations'''
@@ -221,6 +248,7 @@ def stdname2geojson(e, standard_name, min_time, max_time, nchar):
     dfa = adv_search(e, standard_name, cdm_data_type, min_time, max_time)
     if isinstance(dfa, pd.DataFrame):
         datasets = dfa['Dataset ID'].values
+        dpdown_ds.options = datasets
         # find lon,lat values from allDatasets 
         dfll = alllonlat(e, cdm_data_type, min_time, max_time)
         # extract lon,lat values of matching datasets from allDatasets dataframe
@@ -235,7 +263,7 @@ def stdname2geojson(e, standard_name, min_time, max_time, nchar):
 
 # This function updates the time series plot when a station marker is clicked
 
-# In[ ]:
+# In[21]:
 
 def click_handler(event=None, id=None, properties=None):
     datasetID = properties['datasetID']
@@ -248,9 +276,10 @@ def click_handler(event=None, id=None, properties=None):
 
 # This function updates the map when the "refresh" button is selected 
 
-# In[ ]:
+# In[22]:
 
 def button_handler(change):
+    global datasets
     if type(start_time.value)==datetime.date:
         min_time = pendulum.datetime(start_time.value.year, start_time.value.month, start_time.value.day)
     else:
@@ -260,14 +289,15 @@ def button_handler(change):
     else:
         max_time = stop_time.value
     data, datasets = stdname2geojson(e, dpdown.value, min_time, max_time, nchar)
+    dpdown_ds.options = datasets
     feature_layer = ipyl.GeoJSON(data=data)
     feature_layer.on_click(click_handler)
     map.layers = [map.layers[0], feature_layer]
 
 
-# In[ ]:
+# In[23]:
 
-button = ipyw.ToggleButton(
+button = ipyw.Button(
     value=False,
     description='Refresh',
     disabled=False,
@@ -275,18 +305,18 @@ button = ipyw.ToggleButton(
     tooltip='Description')
 
 
-# In[ ]:
+# In[24]:
 
-button.observe(button_handler)
+button.on_click(button_handler)
 
 
 # This function returns the specified dataset time series values as a Pandas dataframe
 
-# In[ ]:
+# In[25]:
 
 def get_data(dataset, standard_name=None, kwargs=None):
     var = e.get_var_by_attr(dataset_id=dataset, 
-                    standard_name=lambda v: str(v).lower() == standard_name)[0]
+                    standard_name=lambda v: str(v).lower() == standard_name.lower())[0]
     download_url = e.get_download_url(dataset_id=dataset, 
                                   variables=['time',var], response='csv', **kwargs)
     df = download_csv(download_url)
@@ -295,7 +325,12 @@ def get_data(dataset, standard_name=None, kwargs=None):
 
 # This defines the initial `ipyleaflet` map 
 
-# In[ ]:
+# In[26]:
+
+dpdown_ds = ipyw.Dropdown()
+
+
+# In[27]:
 
 map = ipyl.Map(center=center, zoom=zoom, layout=ipyl.Layout(width='750px', height='350px'))
 data, datasets = stdname2geojson(e, initial_standard_name, start_time.value, stop_time.value, nchar)
@@ -306,7 +341,7 @@ map.layers = [map.layers[0], feature_layer]
 
 # This defines the intitial `bqplot` time series plot
 
-# In[ ]:
+# In[28]:
 
 dt_x = bq.DateScale()
 sc_y = bq.LinearScale()
@@ -325,32 +360,37 @@ figure.layout.height = '300px'
 figure.layout.width = '800px'
 
 
+# In[29]:
+
+def dpdown_ds_handler(change):
+    datasetID = dpdown_ds.value
+    kwargs = {'time%3E=': start_time.value, 'time%3C=': stop_time.value}
+    df, var = get_data(datasetID, dpdown.value, kwargs)
+    figure.marks[0].x = df.index
+    figure.marks[0].y = df[var]
+    figure.title = '{} - {}'.format(datasetID[:nchar], var)
+    
+
+
+# In[30]:
+
+dpdown_ds.observe(dpdown_ds_handler)
+
+
 # This specifies the widget layout
 
-# In[ ]:
+# In[31]:
 
-from ipywidgets import Layout, Button, Box, FloatText, Textarea, Dropdown, Label, IntSlider
+form_item_layout = ipyw.Layout(display='flex', flex_flow='column', justify_content='space-between')
 
-form_item_layout = Layout(
-    display='flex',
-    flex_flow='column',
-    justify_content='space-between'
-)
-
-
-col1 = Box([map, figure], layout=form_item_layout)
-col2 = Box([dpdown, start_time, stop_time, button], layout=form_item_layout)
-
+col1 = ipyw.Box([map, dpdown_ds, figure], layout=form_item_layout)
+col2 = ipyw.Box([dpdown, start_time, stop_time, button], layout=form_item_layout)
 
 form_items = [col1, col2]
 
-form = Box(form_items, layout=Layout(
-    display='flex',
-    flex_flow='row',
-    border='solid 2px',
-    align_items='flex-start',
-    width='100%'
-))
+form = ipyw.Box(form_items, layout=ipyw.Layout(display='flex', flex_flow='row', border='solid 2px',
+    align_items='flex-start', width='100%'))
+
 form
 
 
